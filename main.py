9efe58f5738553e3eb9501f8ed0ca17c5677de10
siddhant singh <73869528+katariasid565@users.noreply.github.com>
2020-12-01 21:01:26 +0530

    Add files via upload
    this is a very short python application for some strings aur inputs practising

---
+++ main.py
@@ -0,0 +1,9 @@
+# ------------------------------------------
+print("Welcome to the band Generator..!!")
+# ------------------------------------------
+city = input("what's name of the city you grow up in?\n")
+# ------------------------------------------
+pet = input("what's your pet name?\n")
+
+# ------------------------------------------
+print("your band name is " + city + " "+ pet)
